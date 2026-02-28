c264ed74cbd2678484e8a7ade2b686719ced5bd0
pajaykumar <preethikaajay@gmail.com>
2026-03-01 00:52:18 +0100

    Switch to HashRouter to avoid GitHub Pages 404 on refresh

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "./App.css";
 
-const base = import.meta.env.BASE_URL;
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={base}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
