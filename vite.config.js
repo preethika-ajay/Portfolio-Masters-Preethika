b38c5a6226aa7356e985e226159f23b410a97e52
pajaykumar <preethikaajay@gmail.com>
2026-03-01 00:09:18 +0100

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Masters-Preethika/',
-})
+  base: "./",
+});
