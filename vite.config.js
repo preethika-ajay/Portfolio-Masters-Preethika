ed1771504a7e928695bae81a65b21710e9ba4dd6
pajaykumar <preethikaajay@gmail.com>
2026-03-01 00:32:25 +0100

    Fix Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/Portfolio-Masters-Preethika/",
 });
